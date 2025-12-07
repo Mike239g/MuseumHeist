58efed8a85923587ee5546a18b740aafdf7236fa
Miguel239g <127420871+Elinmaculado@users.noreply.github.com>
2025-12-07 14:31:35 -0600

    Camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Originalmente usé una cinemachine freelook camera
+    // Por lo que el movimiento de la cámara ya está hecho ahí
     public float playerSpeed;
+    public Transform camera;
     
     private InputSystem_Actions inputActions;
     private Vector2 movementInput;
@@ -29,13 +32,26 @@ public class PlayerMovement : MonoBehaviour
     public void InputHandle()
     {
         movementInput = inputActions.Player.Move.ReadValue<Vector2>();
-        movement = new Vector3(movementInput.x, 0, movementInput.y);
+        
+        Vector3 camForward = camera.forward;
+        Vector3 camRight = camera.right;
+        
+        camForward.y = 0;
+        camRight.y = 0;
+        
+        camForward.Normalize();
+        camRight.Normalize();
+
+        movement = camForward * movementInput.y + camRight * movementInput.x;
     }
 
     public void MovePlayer()
     {
         if (movementInput.magnitude > 0)
         {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.fixedDeltaTime);
+            
             controller.Move(movement * Time.fixedDeltaTime * playerSpeed);
         }
         
